fbf6b280c5a0a37e79b49cee2c0a5b02d7e774ca
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-19 00:09:23 +0300

    db connection settigs

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./crypto_tracking.db"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
